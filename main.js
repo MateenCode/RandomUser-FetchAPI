d000c07597610abd2e49c7ee26d2f273c7e07108
“mateen1222” <mateenkaziawebdev@gmail.com>
2017-07-11 23:12:50 -0500

    A html Agjacment and css added

--- main.js
+++ main.js
@@ -5,57 +5,46 @@
 
   'use strict';
 
-function getPerson(){
-var img = document.createElement("img")
-var first = document.createElement("h2")
-var last = document.createElement("h3")
-var cell = document.createElement("h1")
-var email = document.createElement("h4")
-var container = document.querySelector(".customers")
+  document.querySelector('body').insertAdjacentHTML('afterbegin', '<h1>INTERNAL COMPANY DIRECTORY</h1>')
+
+  for (var i = 0; i < 12; i++) {
+
+    fetch("https://randomuser.me/api/?nat=us")
+      .then( function(response) {
+        return response.json()
+      })
+      .then( function(json) {
+        console.log("data", json);
+
+        let individual = {}
+        individual.picture = json.results[0].picture.large
+        individual.firstName = json.results[0].name.first
+        individual.lastName = json.results[0].name.last
+        individual.email = json.results[0].email
+        individual.street = json.results[0].location.street
+        individual.city = json.results[0].location.city
+        individual.state = json.results[0].location.state
+        individual.postcode = json.results[0].location.postcode
+        individual.phone = json.results[0].phone
+        individual.social = json.results[0].id.value
 
 
+        let html = `
+          <div class = "customer">
+            <img class = "picture" src="${individual.picture}"></img>
+            <h2 class = "name">${individual.firstName} ${individual.lastName}</h2>
+            <p class = "email">${individual.email}</p>
+            <div class = "address">
+              <p class = "street">${individual.street}</p>
+              <p class = "city">${individual.city}, ${individual.state} ${individual.postcode}</p>
+              <p class = "phone">${individual.phone}</p>
+            </div>
+            <p class = "social">${individual.social}</p>
+          </div>`
 
+        document.querySelector('.customers').insertAdjacentHTML('afterbegin', html);
+      })
 
-fetch("https://randomuser.me/api")
-  .then( function(response){
-    return response.json()
-  })
-  .then(function(json){
-    console.log(json)
-
-
-
-    img.src = json.results[0].picture.large
-
-    first.innerHTML = json.results[0].name.first
-    console.log(first)
-
-    last.innerHTML = json.results[0].name.last
-    console.log(last)
-
-    cell.innerHTML = json.results[0].cell
-    console.log(cell)
-
-    email.innerHTML = json.results[0].email
-    console.log(email)
-
-
-
-    container.appendChild(img)
-    container.appendChild(first)
-    container.appendChild(last)
-    container.appendChild(email)
-
-
-  })
-
-
-
-
-}
-
-for (var j=0; j<=12; j++) {
-  getPerson()
-}
+  }
 
 })();
